20a115c22983fa42af44552541319dda28675842
PC <rita.dudenko@gmail.com>
2020-11-30 10:55:58 +0200

    add links

--- Online_Flower_Shop_(REACT)/src/store/modal/types.js
+++
@@ -1,4 +0,0 @@
-export const CLOSE_MODAL = "CLOSE_MODAL";
-export const CLOSE_MODAL_CART = "CLOSE_MODAL_CART";
-export const OPEN_MODAL = "OPEN_MODAL";
-export const OPEN_MODAL_CART = 'OPEN_MODAL_CART';
